2bc3db5efca56368a7eb86f11ffd2075c31c6ab5
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2023-10-24 21:59:11 +0200

    basic config

---
+++ plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  {
+  "neovim/nvim-lspconfig",
+   config = function()
+     require "plugins.configs.lspconfig"
+     require "custom.configs.lspconfig"
+   end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer"
+      }
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim"
+  },
+}
+
+return plugins
